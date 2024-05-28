4f69f03c7b8b8066d8ac8d57c599c554bc2f0250
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-05-28 13:58:05 +0200

    Add getAuthUrl function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,15 +1,38 @@
-"use strict";
+'use strict';
 
-exports.hello = async (event) => {
+const { google } = require("googleapis");
+const calendar = google.calendar("v3");
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = [
+  "https://vdevhub.github.io/meet/"
+];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+module.exports.getAuthURL = async () => {
+  /**
+   *
+   * Scopes array is passed to the `scope` option. 
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v2.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
   };
 };
