1d3bb7186ccab771aaad3be0ebcc57a8c7e0c605
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-06-20 08:16:36 +0200

    Update for offline use

--- src/api.js
+++ src/api.js
@@ -23,6 +23,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -31,6 +36,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
