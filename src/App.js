6849942199e3b82453805082f88fe8711f781350
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-06-20 14:49:44 +0200

    Add SVG bg and adjust event centering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+import { ReactComponent as BackgroundSVG } from "./background.svg";
 
 const App = () => {
   const [events, setEvents] = useState([]);
