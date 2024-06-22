ef0729d0ce08127afa40275facd8ab2bd3a1845a
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-06-22 14:02:37 +0200

    Recolor events chart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -11,28 +11,28 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
+      const city = location.split((/, | - /))[0]
       return { city, count }; //shorthand of {city: city, count: count}
     })
     return data;
   };
 
   return (
-    //100% causing responsiveness issues
-    <ResponsiveContainer width="99%" height={400}>
+    //100% causing responsiveness issues, use 99% or less
+    <ResponsiveContainer width="99%" height={400} >
       <ScatterChart
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 80,
+          left: 0,
         }}
       >
-        <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />
-        <YAxis type="number" dataKey="count" name="Number of events" />
+        <CartesianGrid fill="#264bba" fillOpacity={0.5} />
+        <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14, stroke: 'white' }} fill="white" />
+        <YAxis type="number" dataKey="count" name="Number of events" tick={{ stroke: 'white' }} />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-        <Scatter name="A school" data={data} fill="#8884d8" />
+        <Scatter name="A school" data={data} fill="cyan" />
       </ScatterChart>
     </ResponsiveContainer>
   );
